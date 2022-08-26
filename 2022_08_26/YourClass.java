26de34a34f126d884b77b0a82c1b481eadfe4e7e
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-08-27 01:33:02 +0900

    2.

--- 2022_08_26/YourClass.java
+++ 2022_08_26/YourClass.java
@@ -1,10 +1,23 @@
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
 
 public class YourClass{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int resultAdder=0;
         for (int i=0; i<args.length; i++){
             resultAdder += Integer.parseInt(args[i]);
         }
+        
+        bw.write("출력: "+ resultAdder);
+        bw.flush();
+        bw.close();
     }
 }
