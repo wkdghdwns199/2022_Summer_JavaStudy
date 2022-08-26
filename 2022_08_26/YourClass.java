074bcee4287a17b985ae50068142cc7e13c8952c
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-08-27 01:48:16 +0900

    5.
    made on off for printing minus value

--- 2022_08_26/YourClass.java
+++ 2022_08_26/YourClass.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -9,8 +10,11 @@ import java.io.BufferedOutputStream;
 
 public class YourClass{
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
+        String setModeForPrintMinus = br.readLine();
+       
         int resultAdder=0;
         int resultSubtractor=Integer.parseInt(args[0]);
 
@@ -23,8 +27,9 @@ public class YourClass{
             resultSubtractor -= Integer.parseInt(args[i]);
         }
         
-        if (resultAdder >= 0) bw.write("출력: "+ resultAdder + "\n");
-        if (resultSubtractor >= 0) bw.write("출력: "+ resultSubtractor + "\n");
+        
+        if (resultAdder >= 0 || setModeForPrintMinus.equals("off")) bw.write("출력: "+ resultAdder + "\n");
+        if (resultSubtractor >= 0 || setModeForPrintMinus.equals("off")) bw.write("출력: "+ resultSubtractor + "\n");
         bw.flush();
         bw.close();
     }
