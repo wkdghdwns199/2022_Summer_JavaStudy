f99ebd7674afdde992c950530c43d74456a888b8
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-08-27 23:39:08 +0900

    6.

--- 2022_08_26/YourClass.java
+++ 2022_08_26/YourClass.java
@@ -12,20 +12,37 @@ public class YourClass{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+    
+
+        bw.write("음수 결과값 출력 모드: ");
+        bw.flush();
+
         String setModeForPrintMinus = br.readLine();
        
-        int resultAdder=0;
-        int resultSubtractor=Integer.parseInt(args[0]);
+        String[] inputStringToNumberArray = args[0].split("\\+");
 
-        for (int i=0; i<args.length; i++){
-            resultAdder += Integer.parseInt(args[i]);
+        int resultAdder=0;
+        int resultSubtractor=Integer.parseInt(inputStringToNumberArray[0]);
+
+        for (int i=0; i<inputStringToNumberArray.length; i++){
+             resultAdder += Integer.parseInt(inputStringToNumberArray[i]);
+         }
+        
+
+        for (int i=1; i<inputStringToNumberArray.length; i++){
+            resultSubtractor -= Integer.parseInt(inputStringToNumberArray[i]);
         }
         
 
-        for (int i=1; i<args.length; i++){
-            resultSubtractor -= Integer.parseInt(args[i]);
-        }
+        // input numbers at command line without character array
+        // for (int i=0; i<args.length; i++){
+        //     resultAdder += Integer.parseInt(args[i]);
+        // }
+        
+
+        // for (int i=1; i<args.length; i++){
+        //     resultSubtractor -= Integer.parseInt(args[i]);
+        // }
         
         
         if (resultAdder >= 0 || setModeForPrintMinus.equals("off")) bw.write("출력: "+ resultAdder + "\n");
