6b6500dafaf6bc1908f9d2ef51e06bc71af569be
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-08-28 19:17:45 +0900

    12.

---
+++ 2022_08_26/MyCalculator.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+public class MyCalculator {
+    
+    public static void main(String[] args) throws IOException{
+        YourClass cal = new YourClass();
+        cal.inputAndCalculate(args);
+           
+    }
+}
--- 2022_08_26/YourClass.java
+++ 2022_08_26/YourClass.java
@@ -10,7 +10,9 @@ import java.io.BufferedOutputStream;
 
 public class YourClass{    
 
-    public static void main(String[] args) throws IOException{
+    public String inputAndCalculate(String[] args) throws IOException{
+
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     
@@ -51,9 +53,8 @@ public class YourClass{
                 inputStringToNumberArray = "0".split("\\+");
             }
         }
-
         int resultAdder=0;
-        int errorOccuredCheck=0;
+        //int errorOccuredCheck=0;
         for (int i=0; i<inputStringToNumberArray.length; i++){
              try{
                 resultAdder += Integer.parseInt(inputStringToNumberArray[i]);
@@ -65,44 +66,44 @@ public class YourClass{
                 }
                 catch (NumberFormatException e2){
                     if (!(inputStringToNumberArray[i].contains("*") && !(inputStringToNumberArray[i].contains("/")) && !(inputStringToNumberArray[i].contains("%")))){
-                        bw.write("출력: 숫자가 아닌 문자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n");
-                        bw.flush();
+                        return "출력: 숫자가 아닌 문자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n";
+                        //bw.write("출력: 숫자가 아닌 문자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n");
+                        //bw.flush();
                     }
                     else {
-                        bw.write("출력: 지원하지 않는 연산자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n");
-                        bw.flush();
+                        return "출력: 지원하지 않는 연산자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n";
+                        //bw.write("출력: 지원하지 않는 연산자가 입력되었습니다! > "+inputStringToNumberArray[i]+"\n");
+                        //bw.flush();
                     }
                     
-                    errorOccuredCheck=1;
+                    
                 }
              }
          }
-        
-        
 
-        // input numbers at command line without character array
-        // for (int i=0; i<args.length; i++){
-        //     resultAdder += Integer.parseInt(args[i]);
-        // }
-        
+         return Integer.toString(resultAdder);
+    }
 
-        // for (int i=1; i<args.length; i++){
-        //     resultSubtractor -= Integer.parseInt(args[i]);
-        // }
+    public static String printResults(String inputCalculationString,String resultString, String setModeForPrintMinus){
         
-        
-        if (errorOccuredCheck==0 && (resultAdder >= 0 || setModeForPrintMinus.equals("off"))) {
-            bw.write("출력커스텀1: "+ resultAdder + "\n");
-            bw.write("출력커스텀2: 정답은 "+ resultAdder + "입니다\n");
-            bw.write("출력커스텀3: " + inputCalculationString + " 의 정답은 " + resultAdder + "입니다\n");
-            
-        
-            bw.flush();
-            bw.close();
+        int resultAdder;
+        try {
+            resultAdder = Integer.parseInt(resultString);
+        }
+        catch(NumberFormatException e){
+            return resultString;
+        }
+
+        if (resultAdder >= 0 || setModeForPrintMinus.equals("off")) {
+            resultString = "출력커스텀1: "+ resultAdder + "\n";
+            resultString += "출력커스텀2: 정답은 "+ resultAdder + "입니다\n";
+            resultString += "출력커스텀3: " + inputCalculationString + " 의 정답은 " + resultAdder + "입니다\n";
         }
         else if (setModeForPrintMinus.equals("on")){
-            bw.write("결과 값이 음수입니다. 음수 결과 값을 보시고 싶으시면 출력 모드를 변환해 주세요!\n");
-            bw.flush();
+            resultString = "결과 값이 음수입니다. 음수 결과 값을 보시고 싶으시면 출력 모드를 변환해 주세요!\n";
+            
         }
+
+        return resultString;
     }
 }
